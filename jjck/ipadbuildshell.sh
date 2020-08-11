98cef3e1315f3073c6d061ca7e108fd87548de9f
zzq <2943917172@qq.com>
2020-08-11 14:53:20 +0800

    优化后的ipad端发布脚本

--- jjck/ipadbuildshell.sh
+++ jjck/ipadbuildshell.sh
@@ -3,9 +3,14 @@
 
 echo "开始运行构建脚本"
 cd /home/gitlab/Z6b9Qc4r/0/mnmqianduan/muniumaweb/ipad
+echo "运行 npm install"
 npm install
+echo "运行 npm run build"
 npm run build
+echo "停止nginx"
 systemctl stop nginx
+echo "复制网站内容到nginx"
 cp -ri dist/* /home/gitlab-runner/project/ipad
+echo "启动nginx"
 systemctl start nginx
 echo "构建脚本运行结束"
