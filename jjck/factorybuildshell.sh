14bbe8737fb32bce6a8a27bf33c3ece152681010
zzq <2943917172@qq.com>
2020-08-13 17:16:33 +0800

    一些小的更改

--- jjck/factorybuildshell.sh
+++ jjck/factorybuildshell.sh
@@ -2,7 +2,7 @@
 # 这个是工厂factory端的发布脚本
 
 echo "开始运行构建脚本"
-cd /home/gitlab-runner/builds/JKQ8p6j5/0/mnmqianduan/muniumaweb/factory
+cd /home/gitlab-runner/builds/AvosHvWw/0/mnmqianduan/muniumaweb/factory
 echo "运行 npm install"
 npm install
 echo "运行 npm run build"
@@ -10,7 +10,7 @@ npm run build
 echo "停止nginx"
 systemctl stop nginx
 echo "复制网站内容到nginx"
-cp -rf dist/* /home/gitlab-runner/project/factory
+cp -rf dist/* /home/publicwebsite/factory
 echo "启动nginx"
 systemctl start nginx
 echo "构建脚本运行结束"
--- jjck/platformbuildshell.sh
+++ jjck/platformbuildshell.sh
@@ -2,7 +2,7 @@
 # 这个是平台platform端的发布脚本
 
 echo "开始运行构建脚本"
-cd /home/gitlab-runner/builds/JKQ8p6j5/0/mnmqianduan/muniumaweb/platform
+cd /home/gitlab-runner/builds/AvosHvWw/0/mnmqianduan/muniumaweb/platform
 echo "运行 npm install"
 npm install
 echo "运行 npm run build"
@@ -10,7 +10,7 @@ npm run build
 echo "停止nginx"
 systemctl stop nginx
 echo "复制网站内容到nginx"
-cp -rf dist/* /home/gitlab-runner/project/platform
+cp -rf dist/* /home/publicwebsite/platform
 echo "启动nginx"
 systemctl start nginx
 echo "构建脚本运行结束"
