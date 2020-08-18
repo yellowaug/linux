bfab3bad4fec4ab80bae97681137f8d5bb70ba91
zzq <2943917172@qq.com>
2020-08-18 09:49:06 +0800

    增加了配置文件覆盖

--- jjck/ipadbuildshell.sh
+++ jjck/ipadbuildshell.sh
@@ -1,6 +1,10 @@
 #! /bin/bash
 # 这个是Ipad端的发布脚本
-
+echo "开始配置正式服配置文件"
+cp -rf /home/hostconfig/ipad/.env* /home/gitlab-runner/builds/ar41oxPj/0/mnmqianduan/muniumaweb/ipad
+cp -rf /home/hostconfig/ipad/request.js /home/gitlab-runner/builds/ar41oxPj/0/mnmqianduan/muniumaweb/ipad/src/utils
+cp -rf /home/hostconfig/ipad/state.js /home/gitlab-runner/builds/ar41oxPj/0/mnmqianduan/muniumaweb/ipad/src/store
+cp -rf /home/hostconfig/ipad/upload.js /home/gitlab-runner/builds/ar41oxPj/0/mnmqianduan/muniumaweb/ipad/src/api
 echo "开始运行构建脚本"
 cd /home/gitlab-runner/builds/ar41oxPj/0/mnmqianduan/muniumaweb/ipad
 echo "运行 npm install"
